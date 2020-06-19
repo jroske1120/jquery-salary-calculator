77a088f47d5d0f903448496c343dd751417bd69a
Joel Roske <joelroske@Owners-MacBook-Air.local>
2020-06-19 16:12:55 -0500

    created inputs, and appended them to the DOM

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Your project description goes here. What problem did you solve? How did you solv
 
 The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
 
+[ x] create form with inputs for first, last name, ID number, job title, salary.
+[ x] submit button that stores information in array
+[ x ] append array info to DOM
+[ ] clear input fields
+[ ] loop through array of objects, if monthly costs > 20000, 
+[ ] if mo/costs > 20k, background color of mo/cost goes red
+[ ] Delete button removes employee from DOM
+
 A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
 Create a delete button that removes an employee from the DOM. For Base mode, it does not need to remove that Employee's salary from the reported total.
--- client.js
+++ client.js
@@ -1,4 +1,46 @@
 console.log('in JS');
 
-$(document).ready();
+$(document).ready(onReady);
 
+let employees = [];
+
+function onReady() {
+    console.log('in onReady');
+    //capture click event
+    $('#submitEmployeeInfo').on('click', addEmployeeInfo);
+} // end onReady
+
+function addEmployeeInfo() {
+    console.log('in addEmployeeInfo');
+    //gather user input and place in an object
+    const newObject = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        IDNumber: $('#IDNumberIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#salaryIn').val()
+    }
+    console.log('newObject:', newObject);
+    employees.push(newObject);//push the object into an array
+    displayAdded();
+    return true;
+}//end addEmployeeInfo
+
+
+function displayAdded() {
+    console.log(' in displayAdded');
+    //target employeeInfoOut by id
+    let el = $('#employeeInfoOut');
+    //empty
+    el.empty();
+    event.preventDefault();
+    //loop through inventory
+    for (i = 0; i < employees.length; i++) {
+        //append each to the DOM
+        el.append(`<li> First Name: ${employees[i].firstName}
+    Last Name: ${employees[i].lastName} 
+    ID Number: ${employees[i].IDNumber}
+    Job Title: ${employees[i].jobTitle}
+    Annual Salary: ${employees[i].annualSalary}</li>`)
+    }
+}
